42d8bca078503a7c517234889926a1181a7d688d
Marfullsen <marfullsen@gmail.com>
2021-03-29 04:51:27 -0300

    Slashes' bugs fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 let patterns = ['black-linen.png', 'black-scales.png', 'black-thread-light.png', 'bo-play.png', 'buried.png', 'carbon-fibre-big.png', 'carbon-fibre-v2.png', 'classy-fabric.png', 'dark-wood.png', 'fake-luxury.png', 'foggy-birds.png', 'football-no-lines.png', 'gplay.png', 'green-fibers.png', 'green-gobbler.png', 'low-contrast-linen.png', 'moulin.png', 'nami.png', 'navy.png', 'otis-redding.png', 'outlets.png', 'pinstripe-light.png', 'random-grey-variations.png', 'real-carbon-fibre.png', 'rebel.png', 'retina-wood.png', 'shley-tree-1.png', 'skulls.png', 'soft-kill.png', 'straws.png', 'subtle-stripes.png', 'swirl.png', 'tasky.png', 'triangles.png', 'white-diamond.png'];
 let currentPattern = [0, 0, 1];
-let ruta = "/assets/img/patterns/";
+let ruta = "assets/img/patterns/";
 const TOTAL_PATTERNS = patterns.length;
 
 function nextPattern(recuadro) {
